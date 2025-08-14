ec7c058335ced4185000c6617d949a2b35fbd6bb
Damien Oriente <damiodev@icloud.com>
2025-08-14 03:57:04 +0200

    🏘️ TOOLTIP FOOTER UPDATED : villes exemples plus précises autour de Montaigut sur Save (Blagnac, Mondonville, Grenade, L'Isle-Jourdain, Lévignac, Daux, Saint-Paul-sur-Save) avec mention 'rayon 20km'

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -98,10 +98,11 @@ const Footer = () => {
                     <div className="group relative">
                       <Info className="h-3 w-3 text-blue-400 cursor-help" />
                       <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-3 py-2 bg-gray-800 text-white text-xs rounded-lg shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none whitespace-nowrap z-10">
-                        <div className="font-medium mb-1">Villes et villages :</div>
-                        <div>Toulouse, L'Isle-Jourdain,</div>
-                        <div>Grenade, Seysses, Muret,</div>
-                        <div>Plaisance-du-Touch, Cugnaux</div>
+                        <div className="font-medium mb-1">Villes exemples (rayon 20km) :</div>
+                        <div>Blagnac, Mondonville,</div>
+                        <div>Grenade, L'Isle-Jourdain,</div>
+                        <div>Lévignac, Daux,</div>
+                        <div>Saint-Paul-sur-Save</div>
                         <div className="absolute top-full left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-4 border-r-4 border-t-4 border-transparent border-t-gray-800"></div>
                       </div>
                     </div>
