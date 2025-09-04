808f52b8d2ac913c03b006400bd16609cc491559
Damien Oriente <damiodev@icloud.com>
2025-09-05 00:50:17 +0200

    fix

--- src/components/SuccessAnimation.tsx
+++ src/components/SuccessAnimation.tsx
@@ -69,7 +69,6 @@ const SuccessAnimation: React.FC<SuccessAnimationProps> = ({
             position: 'absolute',
             left: '50%',
             top: '50%',
-            transform: 'translate(-50%, -50%)',
             width: '128px',
             height: '128px',
             borderRadius: '50%',
